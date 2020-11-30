7190da90e04e07773ca769a2766377de25329e93
Charles de Freitas <me@chasbob.dev>
2020-11-30 17:49:15 +0000

    Add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout, Section } from '../layouts/basic'
+
+export default () => (
+  <Layout title="404">
+    <Section>
+      <h1 className="title is-capitalized has-text-centered is-size-1 is-family-sans-serif">
+        👀 404 👀{' '}
+      </h1>
+    </Section>
+  </Layout>
+)
