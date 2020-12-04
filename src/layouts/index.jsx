2e2e2d4352c86c074e42ed85c7aac3d1f952afae
Charles de Freitas <me@chasbob.dev>
2020-12-04 17:03:32 +0000

    Remove lightbox from layout
    push down to portfolio card images

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -10,29 +10,13 @@ export default ({ children, location }) => {
   if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
     rootPath = __PATH_PREFIX__ + `/`
   }
-  const options = {
-    buttons: {
-      showAutoplayButton: false,
-      showCloseButton: true,
-      showDownloadButton: false,
-      showFullscreenButton: true,
-      showNextButton: false,
-      showPrevButton: false,
-      showThumbnailsButton: false,
-    },
-    thumbnails: {
-      showThumbnails: false,
-    },
-  }
   return (
     <>
       <SEO location={location} />
       <div className="site">
         <SimpleReactLightbox>
           <Navigation />
-          <div className="site-content">
-            <SRLWrapper options={options}>{children}</SRLWrapper>
-          </div>
+          <div className="site-content">{children}</div>
         </SimpleReactLightbox>
       </div>
       <Footer location={location} />
