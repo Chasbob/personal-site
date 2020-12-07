251c04236ec37e51abec67600748b8e342bd1cc6
Charles de Freitas <me@chasbob.dev>
2020-12-07 13:59:31 +0000

    Add contentful about page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,74 @@
-const Promise = require('bluebird')
 const path = require('path')
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.jsx')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulBlogPost {
-              edges {
-                node {
-                  title
-                  slug
+  return graphql(
+    `
+      {
+        allContentfulBlogPost {
+          nodes {
+            title
+            slug
+          }
+        }
+      }
+    `
+  )
+    .then((result) => {
+      if (result.errors) {
+        throw result.errors
+      }
+      const {
+        data: { allContentfulBlogPost },
+      } = result
+      const blogPost = path.resolve('./src/templates/blog-post.jsx')
+      allContentfulBlogPost.nodes.forEach((post, index) => {
+        createPage({
+          path: `/blog/${post.slug}/`,
+          component: blogPost,
+          context: {
+            slug: post.slug,
+          },
+        })
+      })
+    })
+    .then(() =>
+      graphql(`
+        {
+          allContentfulPage {
+            nodes {
+              name
+              path
+              template
+              content {
+                childMarkdownRemark {
+                  html
                 }
               }
             }
           }
-        `
-      ).then((result) => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
         }
-
-        const posts = result.data.allContentfulBlogPost.edges
-        posts.forEach((post, index) => {
-          createPage({
-            path: `/blog/${post.node.slug}/`,
-            component: blogPost,
-            context: {
-              slug: post.node.slug,
-            },
-          })
+      `)
+    )
+    .then((result) => {
+      if (result.errors) {
+        throw result.errors
+      }
+      const {
+        data: { allContentfulPage },
+      } = result
+      allContentfulPage.nodes.forEach((page, index) => {
+        const templatePath = `./src/templates/${page.template || 'default'}.jsx`
+        const component = path.resolve(templatePath)
+        createPage({
+          path: page.path,
+          component,
+          context: { page },
         })
       })
-    )
-  })
+    })
+    .catch((error) => {
+      throw error
+    })
 }
