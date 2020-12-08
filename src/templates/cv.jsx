f88c5329c6730fedea6a7010a95a691a2a219c53
Charles de Freitas <me@chasbob.dev>
2020-12-08 21:58:29 +0000

    Add CV page (#15)
    * Add initial CV template and style
    
    * Add zoom effect
    
    * Update zoom for different breakpoints
    
    * Use zoom cursors

---
+++ src/templates/cv.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { navigate } from 'gatsby'
+import { Section } from '../components/layout/section'
+import styles from './cv.module.scss'
+
+const DefaultTemplate = ({ pageContext }) => {
+  const [wide, setWide] = useState(false)
+  const handleClick = () => setWide(!wide)
+  const {
+    page: {
+      name,
+      content: {
+        childMarkdownRemark: { html },
+      },
+    },
+  } = pageContext
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`${styles.wide} ${(wide && styles.narrow) || ''}`}
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  )
+}
+
+export default DefaultTemplate
