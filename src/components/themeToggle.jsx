a5d39b06ba1637fc54ffce39a2a705cc89236834
Charles de Freitas <me@chasbob.dev>
2020-12-29 15:26:10 +0000

    Update colours for themeToggle button

--- src/components/themeToggle.jsx
+++ src/components/themeToggle.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 
 import useThemeToggle from '../hooks/useThemeToggle'
-import styles from './themeToggle.module.scss'
 
 import { FaMoon, FaSun } from 'react-icons/all'
 
 export default () => {
   const [theme, toggleTheme] = useThemeToggle()
+  let colour
   let icon
   if (theme === 'light') {
     icon = <FaSun key="sun" />
+    colour = 'warning'
   } else {
+    colour = 'dark'
     icon = <FaMoon key="moon" />
   }
 
   return (
-    <button className="button is-warning" onClick={toggleTheme}>
+    <button className={`button is-${colour}`} onClick={toggleTheme}>
       {icon}
     </button>
   )
