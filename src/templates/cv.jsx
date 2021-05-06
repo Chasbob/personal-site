4ced02e610949311736a407d74d47d36928392bb
Charles de Freitas <me@chasbob.dev>
2021-05-06 06:34:58 +0100

    Add embed of CV

--- src/templates/cv.jsx
+++ src/templates/cv.jsx
@@ -1,24 +1,8 @@
 import React, { useState } from 'react'
 import styles from './cv.module.scss'
 
-const DefaultTemplate = ({ pageContext }) => {
-  const [wide, setWide] = useState(false)
-  const handleClick = () => setWide(!wide)
-  const {
-    page: {
-      content: {
-        childMarkdownRemark: { html },
-      },
-    },
-  } = pageContext
-
-  return (
-    <div
-      onClick={handleClick}
-      className={`${styles.cv} ${(wide && styles.narrow) || ''}`}
-      dangerouslySetInnerHTML={{ __html: html }}
-    />
-  )
-}
+const DefaultTemplate = () => (
+  <iframe className={styles.frame} src="https://cv.chasbob.dev/" />
+)
 
 export default DefaultTemplate
