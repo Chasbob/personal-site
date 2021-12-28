53c93b8e2c6bcbc989108de926bcadb866bce909
Charles de Freitas <me@chasbob.dev>
2021-12-28 20:56:53 +0000

    Add custom 500 page

---
+++ src/pages/500.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Section } from '../components/layout/section'
+
+const Error = () => (
+  <>
+    <Section>
+      <h1 className="title is-capitalized has-text-centered is-size-1 is-family-sans-serif">
+        ‼️ 500 ‼️{' '}
+      </h1>
+    </Section>
+  </>
+)
+
+export default Error
