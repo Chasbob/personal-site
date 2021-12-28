6c40243ee7e48f00f33384d98627d4bc9634a041
Charles de Freitas <me@chasbob.dev>
2021-12-28 14:20:02 +0000

    Add SSR for index page images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,19 +3,18 @@ import { graphql } from 'gatsby'
 import Hero from '../components/hero'
 import Social from '../components/social'
 import { Columns } from '../components/layout/column'
-import useThemeToggle from '../hooks/useThemeToggle'
 import Card, {
   CardContent,
   CardHeader,
   CardImage,
 } from '../components/layout/card'
 import { FaGithub, FaLink, FaSpotify } from 'react-icons/fa'
+import { image } from './index.module.scss'
 
-export default function RootIndex({ data }) {
+export default function RootIndex({ data, serverData }) {
   const { allContentfulPerson } = data
   const { edges } = allContentfulPerson
   const [author] = edges
-  const [theme, toggleTheme] = useThemeToggle()
 
   return (
     <>
@@ -24,8 +23,8 @@ export default function RootIndex({ data }) {
         <div className="container">
           <Columns>
             <SocialCard />
-            <Readme theme={theme} />
-            <Spotify />
+            <Readme src={serverData.stats} />
+            <Spotify src={serverData.spotify} />
           </Columns>
         </div>
       </section>
@@ -33,16 +32,15 @@ export default function RootIndex({ data }) {
   )
 }
 
-const Spotify = () => {
+const Spotify = ({ src }) => {
   return (
     <Card fill>
       <CardHeader title="Currently Vibing to..." icon={<FaSpotify />} />
       <CardImage
         image={
-          <img
-            src="https://novatorem.chasbob.vercel.app/api/spotify"
-            alt="Spotify Playing"
-            width="350"
+          <div
+            className={image}
+            dangerouslySetInnerHTML={{ __html: atob(src) }}
           />
         }
       />
@@ -50,17 +48,14 @@ const Spotify = () => {
   )
 }
 
-const Readme = ({ theme }) => (
+const Readme = ({ src }) => (
   <Card fill>
     <CardHeader title="Working on..." icon={<FaGithub />} />
     <CardImage
       image={
-        <img
-          src={`https://github-readme-stats-six-tau.vercel.app/api?username=chasbob&count_private=true&hide_rank=true&hide=stars&hide_title=true&hide_border=true${
-            theme === 'dark' ? '&theme=dark' : ''
-          }`}
-          alt="Github stats"
-          width="350"
+        <div
+          className={image}
+          dangerouslySetInnerHTML={{ __html: atob(src) }}
         />
       }
     />
@@ -97,3 +92,48 @@ export const pageQuery = graphql`
     }
   }
 `
+
+export async function getServerData() {
+  const urlToB64 = (url) =>
+    fetch(url)
+      .then((resp) => resp.blob())
+      .then((blob) => blob.text())
+      .then((txt) => Buffer.from(txt).toString('base64'))
+  try {
+    const stats = await urlToB64(
+      `https://github-readme-stats-six-tau.vercel.app/api?username=chasbob&count_private=true&hide_rank=true&hide=stars&hide_title=true&hide_border=true`
+    )
+    console.log(stats)
+    const spotify = await urlToB64(
+      `https://novatorem.chasbob.vercel.app/api/spotify`
+    )
+    console.log(spotify)
+    return {
+      props: {
+        spotify: spotify,
+        stats: stats,
+      },
+    }
+    const res = await fetch(`https://novatorem.chasbob.vercel.app/api/spotify`)
+    if (!res.ok) {
+      throw new Error(`Response failed`)
+    }
+    // const blob = await res.blob();
+    const blob = await res.blob().then(async (b) => {
+      // const t =
+      const bt = Buffer.from(await b.text()).toString('base64')
+      return bt
+    })
+    return {
+      props: {
+        spotify: blob,
+      },
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      headers: {},
+      props: {},
+    }
+  }
+}
