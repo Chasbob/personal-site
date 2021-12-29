f3cb5dd85c8f606ea0c9cf2c3bd7772181d5af8a
Charles de Freitas <me@chasbob.dev>
2021-12-29 17:45:07 +0000

    Use responsive images in remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,22 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images-contentful`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-transformer-yaml`,
     `gatsby-plugin-layout`,
     {
